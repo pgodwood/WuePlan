7a5336d29b446bfb1cee5f5c2537ba9f42b3ba28
Philip Gottwald <philip.gottwald@web.de>
2021-01-03 22:36:22 +0100

    add basic sql lite db

---
+++ backend/init_db.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+
+with open('db.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content for the second post')
+            )
+
+connection.commit()
+connection.close()
+
+print ("Datenbank erfolgreich erzeugt")
+
+# to create database run:(venv)$ python db_init.py
